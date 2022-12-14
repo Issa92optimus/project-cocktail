ce7c7bd685b883024c11abe9adc7e37b3685877b
Issa Nyale <me@email.com>
2022-12-14 09:07:11 +0300

    completed

---
+++ js/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded",() => {
+    console.log("content loaded")
+})
+
+
+const ulMenu = document.querySelector('.list-group')
+const poster = document.querySelector('img')
+const title = document.querySelector('#title')
+const description = document.querySelector('#description')
+const showtime = document.querySelector('#showTime')
+const availableTickets = document.querySelector('#availableTickets')
+
+function getfilms(data){
+        title.innerText = data.title
+        description.innerText = data.description
+        showtime.innerText = data.showtime
+        let remainingTick = data.capacity - data.tickets_sold
+        availableTickets.innerText = remainingTick
+            poster.src = data.poster 
+}
+
+fetch("http://localhost:3000/films/")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach((film) => {
+        const li = document.createElement('li')
+        li.innerText = film.title
+        ulMenu.appendChild(li)
+
+        li.addEventListener('click', () => {
+            getfilms(film)
+        })
+        })
+            })
+
+
+fetch("http://localhost:3000/films/1")
+    .then(res => res.json())
+    .then(data => {
+            getfilms(data) 
+            })
+
+            
